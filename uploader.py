ff5c62dc47c7694b4bbc1df474b9cb37d3de3061
chai-dev <81156011+chai-dev@users.noreply.github.com>
2021-03-29 09:52:03 +0100

    Updated to say share_bot and changed the bot's image!

--- uploader.py
+++ uploader.py
@@ -1,17 +1,16 @@
 from chai_py import Metadata, package, upload_and_deploy, wait_for_deployment
-from chai_py.deployment import advertise_deployed_bot
+from chai_py import share_bot
 from starter_bot import Bot
-import pyqrcode
 
 DEVELOPER_UID = "developer_unique_id_goes_here"
 DEVELOPER_KEY = "key_goes_here"
 
-bot_image_url = "https://picsum.photos/seed/cool/300/300"
+BOT_IMAGE_URL = "https://cutt.ly/lx0gnM9"
 
 package(
     Metadata(
         name="Your First Bot! 🎉 🤖",
-        image_url=bot_image_url,
+        image_url=BOT_IMAGE_URL,
         color="f1a2b3",
         developer_uid=DEVELOPER_UID,
         description="Thank you for creating me ❤️",
@@ -19,14 +18,11 @@ package(
      )
 )
 
-
 bot_uid = upload_and_deploy(
     "package.zip", uid=DEVELOPER_UID,
     key=DEVELOPER_KEY
 )
+
 wait_for_deployment(bot_uid)
 
-bot_url = advertise_deployed_bot(bot_uid)
-
-url = pyqrcode.create(bot_url)
-print(url.terminal(quiet_zone=1))
+share_bot(bot_uid)
