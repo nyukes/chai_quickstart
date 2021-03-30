5f4a0100bd13fd397411c46c3020b4d4615b97cd
chai-dev <81156011+chai-dev@users.noreply.github.com>
2021-03-30 09:43:48 +0100

    Updated to use set_auth for dev uid and key

--- uploader.py
+++ uploader.py
@@ -1,5 +1,7 @@
 from chai_py import Metadata, package, upload_and_deploy, wait_for_deployment
 from chai_py import share_bot
+from chai_py.auth import set_auth
+
 from starter_bot import Bot
 
 from chai_py.defaults import GUEST_UID, GUEST_KEY
@@ -10,6 +12,7 @@ DEVELOPER_KEY = None
 if DEVELOPER_KEY is None or DEVELOPER_UID is None:
     raise RuntimeError("Please fetch your UID and KEY from the bottom of the Chai Developer Platform. https://chai.ml/dev")
 
+set_auth(DEVELOPER_UID, DEVELOPER_KEY)
 BOT_IMAGE_URL = "https://cutt.ly/lx0gnM9"
 
 package(
@@ -17,15 +20,13 @@ package(
         name="Your First Bot! 🎉 🤖",
         image_url=BOT_IMAGE_URL,
         color="f1a2b3",
-        developer_uid=DEVELOPER_UID,
         description="Thank you for creating me ❤️",
         input_class=Bot,
      )
 )
 
 bot_uid = upload_and_deploy(
-    "package.zip", uid=DEVELOPER_UID,
-    key=DEVELOPER_KEY
+    "package.zip"
 )
 
 wait_for_deployment(bot_uid)
